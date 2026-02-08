c2b293adfa423799e75fcc379de6de3d4f5846fa
DianaCadenaMoreno <diana.marcela.cadena@correounivalle.edu.co>
2026-02-08 00:43:31 -0500

    semantica correcta

--- cypress/e2e/Accesibilidad.cy.js
+++ cypress/e2e/Accesibilidad.cy.js
@@ -2,13 +2,13 @@ import 'cypress-axe';
 
 describe('Accesibilidad', () => {
   beforeEach(() => {
-    //cy.visit('http://localhost:3000'); 
-    cy.visit('https://code-flow.vercel.app/')
+    cy.visit('http://localhost:3000'); 
+    //cy.visit('https://code-flow.vercel.app/')
     cy.injectAxe();
   });
   
   it('debería pasar las pruebas de accesibilidad', () => {
-    cy.get('[data-testid="ContrastIcon"]').should('be.visible').click();
+    // cy.get('[data-testid="ContrastIcon"]').should('be.visible').click();
     cy.checkA11y();
   });
 });
